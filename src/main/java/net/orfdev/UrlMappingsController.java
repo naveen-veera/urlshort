311ea1ade59777ac2025eee8ac755a0de79790dc
Luke Pryor <luke.pryor@orpheussoftware.co.uk>
2018-03-28 09:34:27 +0100

    Fixed comments, removed generated files, upped the gradle wrapper version as it was causing problems when importing to the IDE

--- src/main/java/net/orfdev/UrlMappingsController.java
+++ src/main/java/net/orfdev/UrlMappingsController.java
@@ -19,40 +19,40 @@ public class UrlMappingsController {
 	@RequestMapping("/")
 	public String hello(@RequestParam(defaultValue="World") String who, Model model) {
 		model.addAttribute("who", who);
-		return "hello";
+		return "hello"; // resolves to the HTML template at src/main/resources/templates/hello.html
 	}
 	
-    //Take URL as param shorten url
-    @RequestMapping("/shorten") // defines the URL to access this method
-    @ResponseBody
-    public String shortenUrl(
-    		@RequestParam(value="url", required=true) String longUrl, // the request parameter you can pass to the URL
-    		Model model) { // the Model object is provided by the Spring framework to allow you to pass variables to the template
-    		
-    		Random randomNumber = new Random();
-    		long random = randomNumber.nextLong();
-    		if(random < 0){
-    			random = random * -1;
-    		}
-    		String base62Number = Base62.encode(random);
-    		shortUrlDb.insert(base62Number, longUrl);
-    		
-        return "http://localhost:8080/" + base62Number; // resolves to the HTML template at src/main/resources/templates/greeting.html
-    }
-    
-    @RequestMapping("/{shortUrl}")
-    public String mapUrl(@PathVariable(value="shortUrl") String shortUrl, Model model) {
-    	
-        String longUrl = shortUrlDb.lookup(shortUrl);
-        
-        if(longUrl == null){
-        	System.out.println("Url not Found");
-        	longUrl = "";
-        }
-    	
-        return "redirect:" + longUrl;
-    }
-    
-    // You can add more mappings and methods here!
-    
+	//Take URL as param shorten url
+	@RequestMapping("/shorten") // defines the URL to access this method
+	@ResponseBody // indicates the method will just return the entire response and not use a template
+	public String shortenUrl(
+			@RequestParam(value="url", required=true) String longUrl, // the request parameter you can pass to the URL
+			Model model) { // the Model object is provided by the Spring framework to allow you to pass variables to the template
+			
+		Random randomNumber = new Random();
+		long random = randomNumber.nextLong();
+		if(random < 0) {
+			random = random * -1;
+		}
+		String base62Number = Base62.encode(random);
+		shortUrlDb.insert(base62Number, longUrl);
+			
+		return "http://localhost:8080/" + base62Number;
+	}
+	
+	@RequestMapping("/{shortUrl}")
+	public String mapUrl(@PathVariable(value="shortUrl") String shortUrl, Model model) {
+		
+		String longUrl = shortUrlDb.lookup(shortUrl);
+		
+		if(longUrl == null){
+			System.out.println("Url not Found");
+			longUrl = "";
+		}
+		
+		return "redirect:" + longUrl; // using the "redirect:" prefix tells Spring to return a 301 redirect instead of rendering content
+	}
+	
+	// You can add more mappings and methods here!
+	
 }
